d6d8284126d2420c8a7bbe2a9bbabf4ed1edf276
krameraad <eliaspowder@gmail.com>
2025-12-31 01:18:16 +0100

    input handling progress

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ekramer <ekramer@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/24 00:23:00 by ekramer       #+#    #+#                 */
-/*   Updated: 2025/12/30 21:36:56 by ekramer       ########   odam.nl         */
+/*   Updated: 2025/12/31 01:15:03 by ekramer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ static int	check_badchar(char const **strs)
 		i = 0;
 		while (*strs[i] != '\0')
 		{
-			if (ft_strchr(" \t\n\v\f\r0123456789-", *strs[i]) == NULL)
+			if (ft_strchr(" \t\n\v\f\r0123456789-+", *strs[i]) == NULL)
 				return (true);
 			++i;
 		}
@@ -58,25 +58,54 @@ static int	check_badchar(char const **strs)
 	return (false);
 }
 
+static int	setup_stacks(t_array **stacks, char **strs)
+{
+	int		*array;
+	size_t	i;
+	
+	i = 0;
+	while (strs[i] != NULL)
+		++i;
+	array = malloc(i * sizeof(int));
+	if (array == NULL)
+		return (-1);
+	i = 0;
+	while (strs[i] != NULL)
+	{
+		array[i] = ft_atoi(strs[i]);
+		++i;
+	}
+	if (check_dupes(array, i))
+		return (ft_printf("Error"), free(array), -1);
+	stacks[0] = arr_create(i, array);
+	if (stacks[0] == NULL)
+		return (free(array), -1);
+	stacks[1] = arr_create(i, NULL);
+	if (stacks[1] == NULL)
+		return (arr_free(stacks[0]), -1);
+	return (0);
+}
+
 int	main(int argc, char const *argv[])
 {
 	t_array	*stacks[2];
-	int		*array;
-	char	***strs;
-	size_t	i;
+	char	**strs;
 
 	if (argc == 1)
 		return (0);
 	if (check_badchar(argv + 1))
-		
-	strs = malloc((argc - 1) * sizeof(char**));
+		return (ft_printf("Error"), -1);
+	if (argc == 2)
+		strs = ft_split(argv[1], ' ');
+	else
+		strs = argv;
 	if (strs == NULL)
 		return (-1);
-	i = 0;
-	while (*argv != NULL)
-	{
-		strs[i] = ft_split(argv[i], ' ');
-		++argv;
-	}
+	if (setup_stacks(stacks, strs) == -1)
+		return (free(strs), -1);
+	free(strs);
+	sort(stacks);
+	arr_free(stacks[0]);
+	arr_free(stacks[1]);
 	return (0);
 }
